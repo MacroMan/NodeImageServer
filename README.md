ad7d3c69b6905f678508221756239e853bfcf923
David Wakelin <david@aplpackaging.co.uk>
2017-09-12 13:31:26 +0100

    Added local ramdisk caching and improved serving of images.

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 A lightning fast image processing server with on-the-fly resizing and automatic webp support
 
+## Usage
+
+Update the variable `prefix` in `main.js` to point to the directory containing your images
+
+Update the variable `mainCache` in `main.js` to point to the cache directory, which is recommended to be an ephemeral ramdisk drive for maximum efficiency.
+
+By default, a basic http server is run on port 3000. It is recommended to run a reverse proxy, such as Nginx to serve the content.
+
+If this is the only server running, then you can change the `port` in the `listen(3000)` function at the end of `main.js` to `80` and it will function as a basic webserver on it's own.
+
 ## Motivation
 
 Images today need to be available in any size, fast, and in the best possible file format for the client.
--- main.js
+++ main.js
@@ -3,55 +3,103 @@ var path = require('path');
 var fs = require('fs');
 var url = require('url');
 var sharp = require('sharp');
-var prefix = "../../media"; // Location of the image directory
+var prefix = "../../media";
+var mainCache = "/media/images";
 
 http.createServer(function (req, res) {
 	var accepts = (typeof req.headers.accept != "undefined") ? req.headers.accept.split(",") : [];
-	var webp = (accepts.indexOf("image/webp") >= 0) ? true : false; // Check to see webp is supported by the client
+	var webp = (accepts.indexOf("image/webp") >= 0) ? true : false;
 	var parts = url.parse(req.url, true);
-	var path = prefix + parts.pathname; // Full pathname of the image
 
-	fs.readFile(path, function (err, data) {
-		if (err) { // Image file not found
-			res.statusCode = 404;
-			res.end("404 not found");
-			return false;
+	var width = (typeof parts.query.width != 'undefined') ? parseInt(parts.query.width) : null;
+	if (width < 1 || width > 16383) {
+		width = null;
+	}
+
+	var height = (typeof parts.query.height != 'undefined') ? parseInt(parts.query.height) : null;
+	if (height < 1 || height > 16383) {
+		height = null;
+	}
+
+	var type = (typeof parts.query.type != 'undefined') ? parts.query.type : false;
+
+	var file1 = mainCache + parts.pathname;
+	file1 += (width) ? '.w' + width : '';
+	file1 += (height) ? '.h' + height : '';
+	if (type == 'png') {
+		file1 += '.' + type;
+	} else if (webp) {
+		file1 += '.webp';
+	}
+
+	var file2 = mainCache + parts.pathname;
+	var file3 = prefix + parts.pathname;
+
+	console.log(file1);
+	console.log(file2);
+	console.log(file3);
+
+	[file1, file2].forEach(function (file) {
+		var path = file.split('/');
+		path.pop();
+		path = path.join('/');
+		if (!fs.existsSync(path)) {
+			fs.mkdirSync(path);
 		}
+	});
 
-		var image = sharp(data);
-		// Get the URL query values for width and height
-		var width = (typeof parts.query.width != 'undefined') ? parseInt(parts.query.width) : null;
-		var height = (typeof parts.query.height != 'undefined') ? parseInt(parts.query.height) : null;
+	if (fs.existsSync(file1)) {
+		serveStatic(file1);
+	} else if (fs.existsSync(file2)) {
+		processImage(file2);
+	} else if (fs.existsSync(file3)) {
+		processImage(file3, true);
+	} else {
+		res.statusCode = 404;
+		res.end("404 not found");
+	}
 
-		// Make sure width and height are within range
-		if (width < 1 || width > 16383) {
-			width = null;
-		}
-		if (height < 1 || height > 16383) {
-			height = null;
-		}
-
-		// Resize, sharpen and set quality
-		image.resize(width, height).max();
-		image.sharpen();
-		image.quality(95);
-
-		if (webp) { // Return webp file if supported by client
-			image.webp();
+	function serveStatic(path) {
+		if (type == 'png') {
+			res.setHeader("Content-Type", "image/png");
+		} else if (webp) {
 			res.setHeader("Content-Type", "image/webp");
-		} else { // or return jpeg
-			image.jpeg();
+		} else {
 			res.setHeader("Content-Type", "image/jpeg");
 		}
+		fs.createReadStream(path).pipe(res);
+	}
 
-		image.toBuffer(function (err, buffer, info) { // Buffer the image file ready to send
+	function processImage(path, cache) {
+		fs.readFile(path, function (err, data) {
 			if (err) {
-				res.statusCode = 500;
-				res.end("500 internal server error");
-				return false;
+				res.statusCode = 404;
+				res.end("404 not found");
+				return;
+			}
+
+			var image = sharp(data);
+			image.resize(width, height).max();
+			image.sharpen();
+			image.normalise();
+			image.quality(96);
+			if (type == 'png') {
+				image.png({adaptiveFiltering: false});
+				res.setHeader("Content-Type", "image/png");
+			} else if (webp) {
+				image.webp();
+				res.setHeader("Content-Type", "image/webp");
 			} else {
-				image.pipe(res); // Pipe the image out
+				image.background({r: 255, g: 255, b: 255, alpha: 1}).flatten().jpeg();
+				res.setHeader("Content-Type", "image/jpeg");
+			}
+
+			image.pipe(fs.createWriteStream(file1));
+			image.pipe(res);
+
+			if (cache) {
+				fs.createReadStream(path).pipe(fs.createWriteStream(file2));
 			}
 		});
-	});
+	}
 }).listen(3000);
